dd578027dea895556d096bda43a56a89959090a4
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 15:04:26 -0400

    create post path  for page create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/pages', to: 'pages#index'
+post '/pages', to: 'pages#create'
 get '/pages/new', to: 'pages#new', as: 'new_page'
 get '/pages/:id', to: 'pages#show', as: 'page'
 
