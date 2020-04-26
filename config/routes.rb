1f6fdfd17ce9720fe49f94ac9404c943802c09b9
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 14:29:34 -0400

    create '/new' route

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/pages', to: 'pages#index'
 get '/pages/:id', to: 'pages#show', as: 'page'
+get '/pages/new', to: 'pages#new'
 
 end
