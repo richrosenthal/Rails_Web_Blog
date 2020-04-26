da1141df54c4c11588cd6e67f71dca32aba252a1
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 15:31:56 -0400

    create edit route

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/pages', to: 'pages#index'
 post '/pages', to: 'pages#create'
+
 get '/pages/new', to: 'pages#new', as: 'new_page'
 get '/pages/:id', to: 'pages#show', as: 'page'
-
+get '/pages/:id/edit', to: 'pages#edit'
 
 end
