33cc02a27452cf862266ead5aa218b9f2d15a1ff
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 15:46:15 -0400

    create patch route

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ post '/pages', to: 'pages#create'
 get '/pages/new', to: 'pages#new', as: 'new_page'
 get '/pages/:id', to: 'pages#show', as: 'page'
 get '/pages/:id/edit', to: 'pages#edit', as: 'edit_page'
+patch 'pages/:id', to: 'pages#update'
 
 end
