789f9eeb0438230abda353568ccbb82b9283d290
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 14:59:59 -0400

    create new page path

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/pages', to: 'pages#index'
-get '/pages/new', to: 'pages#new'
+get '/pages/new', to: 'pages#new', as: 'new_page'
 get '/pages/:id', to: 'pages#show', as: 'page'
 
 
