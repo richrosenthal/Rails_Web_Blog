aa965b37d02c24f8dc72f40de9e0db42530e09c2
richrosenthal <rrosenthal2013@gmail.com>
2020-04-26 14:30:19 -0400

    update route priorities

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 get '/pages', to: 'pages#index'
-get '/pages/:id', to: 'pages#show', as: 'page'
 get '/pages/new', to: 'pages#new'
+get '/pages/:id', to: 'pages#show', as: 'page'
+
 
 end
