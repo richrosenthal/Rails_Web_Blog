3c678bd7e818484e9cd642460be9deda3179848a
richrosenthal <rrosenthal2013@gmail.com>
2020-04-27 12:53:31 -0400

    refactor

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,24 @@
 class CommentsController < ApplicationController
+  before_action :set_post
+  
   def create
-    @post = Post.find(params[:post_id])
+
     @comment = @post.comments.create(comment_params)
     redirect_to @post
   end
 
+  def edit
+
+    @comment = Comment.find(params[:id])
+  end
+
+
   private
     def comment_params
       params.require(:comment).permit(:name, :content)
+    end
+
+    def set_post
+      @post = Post.find(params[:post_id])
     end
 end
