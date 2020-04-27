0253a650385ff3c7a4c3233cb0675ddc116622dd
richrosenthal <rrosenthal2013@gmail.com>
2020-04-27 11:19:32 -0400

    migrate db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_015934) do
+ActiveRecord::Schema.define(version: 2020_04_27_151650) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
